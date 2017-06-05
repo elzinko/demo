2d1eb0c58c238632c955913cb40973db503e00a1
elzinko <thomas.couderc@gmail.com>
2017-06-06 00:38:55 +0200

    #1 maven spring boot dockerized and rancherized

--- devel/01-java-build-run.sh
+++
@@ -1,1 +0,0 @@
-mvn package && java -jar target/demo*.jar
--- devel/02-docker-build-run.sh
+++ devel/02-docker-build-run.sh
@@ -1,2 +1,2 @@
-docker build -t demo:1.0 .
-docker run -d --name demo -p 8080:8080 demo:1.0
+docker build -t demo:latest .
+docker run -d --name demo -p 8080:8080 demo:latest
