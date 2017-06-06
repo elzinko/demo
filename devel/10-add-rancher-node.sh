32c30a36abad3288ad7b40a1af9222fe5270736d
elzinko <thomas.couderc@gmail.com>
2017-06-06 11:42:51 +0200

    #4 scale node

---
+++ devel/10-add-rancher-node.sh
@@ -0,0 +1,3 @@
+echo "start node2 + activate it on rancher"
+echo "see lb and cadvisor auto deploy"
+echo "add constraints to demo to avoid multiple instance per node => \${stack_name}/\${service_name}"
